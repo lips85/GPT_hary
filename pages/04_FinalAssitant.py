02a5c0aa4bb8925234723fa266a8a0756b6cb341
haru <godjaewook@naver.com>
2024-09-16 05:42:21 +0900

    변경

--- pages/04_FinalAssitant.py
+++ pages/04_FinalAssitant.py
@@ -131,9 +131,9 @@ class IssueSearchClient:
 
     def save_file(self, inputs):
         text = inputs["text"]
-        with open("research_report.txt", "w", encoding="utf-8") as f:
+        with open("final_result.txt", "w", encoding="utf-8") as f:
             f.write(text)
-        st.download_button(label="다운로드", file_name="final.txt_result", data=text)
+        st.download_button(label="다운로드", file_name="final_result.txt", data=text)
         return "저장 완료"
 
 
